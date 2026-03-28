cad40968e8e6404c6338e36bc2bdfaabe5663408
Marcos <96637354+Marcosmanoel1@users.noreply.github.com>
2026-03-28 16:17:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def send_menu(chat_id, text):
 
 
 def gemini(system_prompt, user_message):
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={GEMINI_API_KEY}"
     payload = {
         "system_instruction": {
             "parts": [{"text": system_prompt}]
